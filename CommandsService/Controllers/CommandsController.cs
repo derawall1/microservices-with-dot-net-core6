ad9c775a462e42913657ba8f8d7753a0750658a6
derawall1 <derawall@gmail.com>
2022-05-26 15:08:32 +0500

    Add project files.

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    
+    public class CommandsController : ControllerBase
+    {
+        
+    }
+}
